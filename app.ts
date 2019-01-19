c59441eab7407d19a7dfed7976f9956df35784ce
Walter Bormans <bronzdragon@gmail.com>
2019-01-20 00:18:36 +0100

    Adds minimal parsing of types.

--- app.ts
+++ app.ts
@@ -5,12 +5,33 @@ import * as fs from "fs";
 // const fs = require('fs'), filename = process.argv[2];
 fs.readFile("./datamodel.prisma", "utf8", function(err :any , data: string): void {
   if (err) { throw err; }
-  console.log(data);
+  // console.log(data);
+
   let schema: Schema = new Schema(data);
+
 });
 
 class Schema {
-  constructor(baseText: string) {
-    // do stuff
+  private models : Array<Model>;
+
+  constructor(datamodel_text: string) {
+    const regex: RegExp = /type\s*(\w+)\s*\{([\s\S]+?)\}/gm;
+
+
+    let type: Array<string>;
+    while(type = regex.exec(datamodel_text)) {
+      this.models.push(new Model(type[1], type[2]));
+
+      console.log("Entry: ");
+      console.log(`Name: ${type[1]}\nBody:\n${type[2]}`);
+    }
+
+  }
+}
+
+class Model {
+  public readonly name: string;
+  constructor(name: string, body: string) {
+    this.name = name;
   }
 }
